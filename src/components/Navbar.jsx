6c7947cddfdc53243f0ee4657878d4452b96ee4c
Kelly Chara <fabianachara@gmail.com>
2023-03-31 22:03:05 -0500

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,15 @@
 export const Navbar = () => {
   return (
     <nav className="flex justify-around items-center py-2 bg-transparent">
-      <figure className="hover:scale-105 cursor-pointer">
-        <img src="/assets/logo.png" alt="logo" />
+      <figure className="hover:scale-105 cursor-pointer w-10">
+        <img src="/assets/logo.svg" alt="logo" />
       </figure>
 
       <ul className="flex gap-6 items-center">
-        <li className="hover:scale-105 cursor-pointer underline text-white text-xs font-bold underline-offset-1">
+        <li className="nav-links">
           <a href="#history">01. HISTORY</a>
         </li>
-        <li className="hover:scale-105 cursor-pointer underline text-white text-xs font-bold underline-offset-1">
+        <li className="nav-links">
           <a href="#team">02. TEAM</a>
         </li>
       </ul>
